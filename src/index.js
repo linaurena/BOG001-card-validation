da0b37a337ac38d8c359392584eb6f4cd99b8306
linaurena <lina8maria8@hotmail.com>
2021-02-04 22:06:06 -0500

    Catch event from input

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ console.log(validator);
 
 const logo = document.getElementById('logo');
 const onboardingView = document.getElementById('onboarding');
-const cardView = document.getElementById('card-view');
 const btnStart = document.getElementById('start');
+const cardView = document.getElementById('card-view');
 
 btnStart.addEventListener('click', function(){
   onboardingView.classList.add('hidden');
@@ -17,4 +17,20 @@ btnStart.addEventListener('click', function(){
 logo.addEventListener('click', function(){
   cardView.classList.add('hidden');
   onboardingView.classList.remove('hidden');
+})
+
+// Input
+
+const creditCardNumber = document.getElementById('input');
+
+creditCardNumber.addEventListener('keyup', (e) => {
+  console.log(e)
+})
+
+// Check it btn
+
+const checkBtn = document.getElementById('check');
+
+checkBtn.addEventListener('click', function(){
+  console.log('yeee');
 })
